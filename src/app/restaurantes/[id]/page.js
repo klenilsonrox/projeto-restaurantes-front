2529aadfb0f922e889170386e628ae9ce9ae2d8c
klenilsonrox <klenilsoncoc@gmail.com>
2024-07-03 17:40:48 -0300

    att

--- src/app/restaurantes/[id]/page.js
+++ src/app/restaurantes/[id]/page.js
@@ -34,7 +34,11 @@ const RestaurantDetails = ({params}) => {
 
     return (
         <div className='max-w-7xl mx-auto p-4'>
-            <h1 className="text-3xl font-semibold py-10 flex items-center gap-4"> <img src={restaurant.urlImage} alt="" className='w-20 h-20 object-cover rounded-full'/> <span>{restaurant.name}</span> </h1>
+           <div className='flex justify-between items-center'>
+           <h1 className="text-3xl font-semibold py-10 flex items-center gap-4"> <img src={restaurant.urlImage} alt="" className='w-20 h-20 object-cover rounded-full'/> <span>{restaurant.name}</span> </h1>
+           <Link href={`/`} className='font-bold underline text-blue-700'> ← voltar</Link>
+           </div>
+
             <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {products && products.map(product => (
                     <div key={product._id} className="rounded-lg overflow-hidden transition-all shadow-md hover:shadow-lg">
@@ -47,7 +51,7 @@ const RestaurantDetails = ({params}) => {
                     </div>
                 ))}
             </div>
-            <Link href="/" className='py-6 block'>Voltar para a lista de restaurantes</Link>
+
         </div>
     );
 };
